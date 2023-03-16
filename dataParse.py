88eec5da88811cc968253c3e158d0f57f43911ba
sa2006026 <sa200602687@yahoo.com>
2023-03-16 17:49:22 +0800

    Modified for easier understanding

--- dataParse.py
+++ dataParse.py
@@ -1,57 +1,47 @@
-#Instructions on how to install venv
-#Open file location in terminal
-#Run the following commands
-#1) python3 -m venv .env
-#2) .env/bin/activate
-#3) pip install -r requirements.txt
-#4) python3 dataParse.py
+# Instructions on how to install venv
+# Open file location in terminal
+# Run the following commands
+# 1) python3 -m venv .env
+# 2) .env/bin/activate
+# 3) pip install -r requirements.txt
+# 4) python3 dataParse.py
 
 import numpy as np
 import scipy.signal as sig
 
-filepath = "data/new_sensor_2sec_on_2min_off_3"
+filepath = "data/new_sensor_2sec_on_2min_off_3"  # Change it before running code
 
-data = np.genfromtxt(filepath + ".txt", delimiter = ",", names = True)
+data = np.genfromtxt(filepath + ".txt", delimiter=",", names=True)
 
-#save the first coloumn and secound column as timestamp and resitance
+# save the first coloumn and secound column as timestamp and resitance
 timestamp = data['timestamp']
 resistance = data['S15']
 
-idxs, properties = sig.find_peaks(resistance, 
-                     width = 1700, 
-                     distance = 1000,
-                     height = 54000)
+idxs, properties = sig.find_peaks(resistance,
+                                  width=1700,
+                                  distance=1000,
+                                  height=54000)
 
-rnge = np.zeros([len(idxs), 1], dtype=int)
+start_idx = np.zeros([len(idxs), 1], dtype=int)
 for i in range(len(idxs)):
-    #Finding Peak Start
-    """tmp = idxs[i]
-    while(True):
-        if base - 2 < raw[tmp] and base + 2 > raw[tmp]: # if the value is within +/- 2 of the base value (right of the peak)
-            rnge[i][0] = tmp
-            break
-        tmp -= 1"""
-    #Finding Peak End
+    # Find the trough to the left of the peak
     tmp = idxs[i]
     for j in range(1500):
-        if resistance[rnge[i]] > resistance[tmp]: # if the value is within +/- 2 of the base value (left of the peak)
-            rnge[i] = tmp
+        # if the value is within +/- 2 of the base value (left of the peak)
+        if resistance[rnge[i]] > resistance[tmp]:
+            start_idx[i] = tmp
         tmp += 1
         if tmp == len(resistance):
-            rnge[i] = len(resistance) - 1
+            start_idx[i] = len(resistance) - 1
             break
-#print(rnge)
+
 
 ratio = np.zeros([len(idxs), 1], dtype=float)
 
 for i in range(len(idxs)):
-    ratio[i] = resistance[idxs[i]] / resistance[rnge[i]]
+    ratio[i] = resistance[idxs[i]] / resistance[start_idx[i]]
 
-#save ratio as a csv file and text file
+# save ratio as a csv file and text file
 save_file = filepath + "_ratio"
-np.savetxt(save_file+".txt", ratio, delimiter = ",")
-np.savetxt(save_file+".csv", ratio, delimiter = ",")
-
-
-
-
+np.savetxt(save_file+".txt", ratio, delimiter=",")
+np.savetxt(save_file+".csv", ratio, delimiter=",")
